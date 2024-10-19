4a7766da2c01c0b85628a0c8ca6ceb59e992818b
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-18 23:13:10 -0300

    Created the pokemon entity

---
+++ src/domain/entities/pokemon.entity.ts
@@ -0,0 +1,21 @@
+export class Pokemon {
+    id: number;
+    name: string;
+    base_experience: number;
+    height: number;
+    is_default: boolean;
+    order: number;
+    weight: number;
+    abilities: any[];
+    forms: any[];
+    game_indices: any[];
+    held_items: any[];
+    location_area_encounter: string;
+    moves: any[];
+    past_types: any[];
+    sprites: any[];
+    cries: any[];
+    species: any[];
+    stats: any[];
+    types: any[];
+}
