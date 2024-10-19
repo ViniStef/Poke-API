759426b8250eb836021fd01c57667d0af8f356ed
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-19 19:45:13 -0300

    Created the pokemon repository inside the infrastructure

---
+++ src/infrastructure/repositories/pokemon.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { IPokemonRepository } from '../../domain/repositories/ipokemon.repository';
+import { Pokemon } from '../../domain/entities/pokemon.entity';
+
+@Injectable()
+export class PokemonApiService implements IPokemonRepository {
+    async findByIdOrName(id?: number, name?: string): Promise<Pokemon> {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id ?? name}`;
+        const response = await axios.get<Pokemon>(url);
+        return response.data;
+    }
+
+    async findByColor(color: string): Promise<Pokemon[]> {
+        const url = `https://pokeapi.co/api/v2/pokemon-color/${color}`;
+        const response = await axios.get(url);
+        return response.data.pokemon.map(p => p.pokemon);
+    }
+
+    async findPaginated(limit: number, offset: number): Promise<Pokemon[]> {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+        const response = await axios.get(url);
+        return response.data.results;
+    }
+}
