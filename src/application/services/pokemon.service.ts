797504d17811b357162184455e577f7526f8b4be
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-20 01:07:23 -0300

    Added the pokemon service

--- src/application/services/pokemon.service.ts
+++ src/application/services/pokemon.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { IPokemonRepository } from '../../domain/repositories/ipokemon.repository';
 import { Pokemon } from '../../domain/entities/pokemon.entity';
+import {PokemonRepository} from "../../infrastructure/repositories/pokemon.repository";
 
 @Injectable()
 export class PokemonService {
-    constructor(private readonly pokemonRepository: IPokemonRepository) {}
+    constructor(private readonly pokemonRepository: PokemonRepository) {}
 
-    async getPokemonByIdOrName(id?: number, name?: string): Promise<Pokemon> {
-        return this.pokemonRepository.findByIdOrName(id, name);
+     async getPokemonByIdOrName(pokemonParam: string): Promise<Pokemon> {
+        const id = Number(pokemonParam);
+
+        if (!isNaN(id)) {
+            return await this.pokemonRepository.findByIdOrName(id, undefined);
+        } else {
+            return await this.pokemonRepository.findByIdOrName(undefined, pokemonParam);
+        }
     }
+
+    async getPokemonsByColor(colorParam: string): Promise<Pokemon[]> {
+        return await this.pokemonRepository.findByColor(colorParam);
+    }
+
+    async getPaginatedPokemons(limit: string, offset: string): Promise<Pokemon[]> {
+        const limitConverted = Number(limit);
+        const offsetConverted = Number(offset);
+
+        return await this.pokemonRepository.findPaginated(limitConverted, offsetConverted);
+    }
+
 }
