d672feaff64bfde445942ecfabb38f79a770103e
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-20 20:33:35 -0300

    Updated service with error handling for all routes

--- src/application/services/pokemon.service.ts
+++ src/application/services/pokemon.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
-import { Pokemon } from '../../domain/entities/pokemon.entity';
+import {BadRequestException, Injectable, InternalServerErrorException, NotFoundException} from '@nestjs/common';
+import {Pokemon} from '../../domain/entities/pokemon.entity';
 import {PokemonRepository} from "../../infrastructure/repositories/pokemon.repository";
+import {PokemonColorInfo} from "../../domain/entities/pokemon-color.entity";
+import {PokemonPaginated} from "../../domain/entities/pokemon-paginated.entity";
+import {AxiosError} from "axios";
 
 @Injectable()
 export class PokemonService {
-    constructor(private readonly pokemonRepository: PokemonRepository) {}
+    constructor(private readonly pokemonRepository: PokemonRepository) {
+    }
 
-     async getPokemonByIdOrName(pokemonParam: string): Promise<Pokemon> {
+    async getPokemonByIdOrName(pokemonParam: string): Promise<Pokemon> {
         const id = Number(pokemonParam);
+        if (id < 0) {
+            throw new BadRequestException({
+                message: "The Pokemon ID cannot be negative.",
+                status: 400
+            })
+        }
+        try {
+            if (!isNaN(id)) {
+                return await this.pokemonRepository.findByIdOrName(id, undefined);
+            }
 
-        if (!isNaN(id)) {
-            return await this.pokemonRepository.findByIdOrName(id, undefined);
-        } else {
             return await this.pokemonRepository.findByIdOrName(undefined, pokemonParam);
+
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                if (error.status === 404) {
+                    throw new NotFoundException(
+                        {
+                            message: "Pokemon not found in the PokeAPI, please try with a different id or name.",
+                            status: 404
+                        });
+                } else {
+                    throw new InternalServerErrorException(
+                        {
+                            message: "Unexpected error",
+                            status: 500
+                        }
+                    )
+                }
+            }
         }
     }
 
-    async getPokemonsByColor(colorParam: string): Promise<Pokemon[]> {
-        return await this.pokemonRepository.findByColor(colorParam);
+    async getPokemonsAndColorInfoByColor(colorParam: string): Promise<PokemonColorInfo> {
+        if (parseInt(colorParam) < 0) {
+            throw new BadRequestException({
+                message: "The Color ID cannot be negative.",
+                status: 400
+            })
+        }
+        try {
+            return await this.pokemonRepository.findByColor(colorParam);
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                if (error.status === 404) {
+                    throw new NotFoundException(
+                        {
+                            message: "Color not found in the PokeAPI, please try with a different id or color name.",
+                            status: 404
+                        });
+                } else {
+                    throw new InternalServerErrorException(
+                        {
+                            message: "Unexpected error",
+                            status: 500
+                        }
+                    )
+                }
+            }
+        }
     }
 
-    async getPaginatedPokemons(limit: string, offset: string): Promise<Pokemon[]> {
+    async getPaginatedPokemons(limit: string, offset: string): Promise<PokemonPaginated> {
         const limitConverted = Number(limit);
         const offsetConverted = Number(offset);
 
