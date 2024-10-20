4b9b3262bc73a873fb9f854fb6fa0951da927fc8
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-20 01:12:22 -0300

    Created the pokemon repository inside the infrastructure

--- src/infrastructure/repositories/pokemon.repository.ts
+++ src/infrastructure/repositories/pokemon.repository.ts
@@ -5,21 +5,21 @@ import { Pokemon } from '../../domain/entities/pokemon.entity';
 
 @Injectable()
 export class PokemonRepository implements IPokemonRepository {
-    async findByIdOrName(id?: number, name?: string): Promise<Pokemon> {
+    async findByIdOrName(id: number, name: string): Promise<Pokemon> {
         const url = `https://pokeapi.co/api/v2/pokemon/${id ?? name}`;
         const response = await axios.get<Pokemon>(url);
         return response.data;
     }
 
-    async findByColor(color: string): Promise<Pokemon[]> {
-        const url = `https://pokeapi.co/api/v2/pokemon-color/${color}`;
+    async findByColor(colorParam: string): Promise<Pokemon[]> {
+        const url = `https://pokeapi.co/api/v2/pokemon-color/${colorParam}`;
         const response = await axios.get(url);
-        return response.data.pokemon.map(p => p.pokemon);
+        return response.data;
     }
 
     async findPaginated(limit: number, offset: number): Promise<Pokemon[]> {
         const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
         const response = await axios.get(url);
-        return response.data.results;
+        return response.data;
     }
 }
