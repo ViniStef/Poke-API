3a258fa261ea76159a533aeebee98367d0808c77
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-20 01:19:38 -0300

    Updated the app module with the new pokemon module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PokemonModule } from "./application/modules/pokemon.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PokemonModule],
 })
+
 export class AppModule {}
