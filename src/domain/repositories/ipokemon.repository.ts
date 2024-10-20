95a8a27b4fd38819be731b52c60fed451f231cb3
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-20 20:34:38 -0300

    Updated the repository interface with the new entities

--- src/domain/repositories/ipokemon.repository.ts
+++ src/domain/repositories/ipokemon.repository.ts
@@ -1,7 +1,9 @@
 import { Pokemon } from '../entities/pokemon.entity';
+import {PokemonColorInfo} from "../entities/pokemon-color.entity";
+import {PokemonPaginated} from "../entities/pokemon-paginated.entity";
 
 export interface IPokemonRepository {
     findByIdOrName(id?: number, name?: string): Promise<Pokemon>;
-    findByColor(color: string): Promise<Pokemon[]>;
-    findPaginated(limit: number, offset: number): Promise<Pokemon[]>;
+    findByColor(color: string): Promise<PokemonColorInfo>;
+    findPaginated(limit: number, offset: number): Promise<PokemonPaginated>;
 }
